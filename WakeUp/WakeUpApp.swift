fb9f74e11894db5bfb5fec8156a8e52911333edc
SanShamor <sanshamor@mail.ru>
2022-07-15 15:14:10 +0300

    Initial Commit

---
+++ WakeUp/WakeUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeUpApp.swift
+//  WakeUp
+//
+//  Created by Александр on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WakeUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
