46e475d1d6ee35be1f8c3f4b376a0c89b4bdc1d8
SanShamor <sanshamor@mail.ru>
2022-07-19 13:08:28 +0300

    Start animation in SwiftUI

--- WakeUp/ContentView.swift
+++ WakeUp/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isToggleOn = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.blue)
+                    .offset(x: isToggleOn ? 200 : 0)
+            Text("SwiftUI testing")
+                .offset(x: isToggleOn ? 200 : 0)
+            }
+            Toggle(isOn: $isToggleOn, label: {
+                Text("Hide info")
+            })
+        }
+        .padding()
+        .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.4), value: isToggleOn)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
