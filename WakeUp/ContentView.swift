d3d02f674b5b630360bb1610405b5095fd8d141d
SanShamor <sanshamor@mail.ru>
2022-07-19 19:20:49 +0300

    add NavigationView and Picker

--- WakeUp/ContentView.swift
+++ WakeUp/ContentView.swift
@@ -9,22 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     @State var isToggleOn = false
+    @State var section = 0
+    
+    @State var numberOfPeople = 2
+    
+    var settings = ["First way", "Second way", "Third way"]
     
     var body: some View {
         VStack {
+            Toggle(isOn: $isToggleOn, label: {
+                Text("Show preferences")
+            })
             ZStack {
+                NavigationView {
+                    Form {
+                        Section {
+                            Picker("Number of KEK", selection: $numberOfPeople) {
+                                ForEach(2..<5) {
+                                    Text("\($0) kek")
+                                }
+                            }
+                        }
+                    }
+                }
+                
                 RoundedRectangle(cornerRadius: 10)
                     .fill(Color.blue)
-                    .offset(x: isToggleOn ? 200 : 0)
-            Text("SwiftUI testing")
-                .offset(x: isToggleOn ? 200 : 0)
+                    .offset(x: isToggleOn ? UIScreen.main.bounds.size.width : 0)
+                Text("SwiftUI testing. Turn on toggle")
+                    .offset(x: isToggleOn ? UIScreen.main.bounds.size.width : 0)
             }
-            Toggle(isOn: $isToggleOn, label: {
-                Text("Hide info")
-            })
+            .padding()
         }
         .padding()
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.4), value: isToggleOn)
+   
     }
 }
 
